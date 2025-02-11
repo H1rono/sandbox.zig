7dc79fb7083ddd8f480f009c051e8bd1484b9acb
H1rono <hronok66@gmail.com>
2025-02-12 02:59:47 +0900

    :wrench: Add `hello-world` step

--- build.zig
+++ build.zig
@@ -1,12 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const helloWorld = b.addExecutable(.{
-        .name = "hello-world",
-        .root_source_file = b.path("hello-world/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
-        .single_threaded = true,
-    });
-    b.installArtifact(helloWorld);
+    // hello-world
+    {
+        const exe = b.addExecutable(.{
+            .name = "hello-world",
+            .root_source_file = b.path("hello-world/main.zig"),
+            .target = b.standardTargetOptions(.{}),
+            .optimize = b.standardOptimizeOption(.{}),
+            .single_threaded = true,
+        });
+        b.installArtifact(exe);
+
+        const run_exe = b.addRunArtifact(exe);
+        const run_step = b.step("hello-world", "Run the hello-world app");
+        run_step.dependOn(&run_exe.step);
+    }
 }
