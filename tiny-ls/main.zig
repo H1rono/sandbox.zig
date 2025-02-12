9b5d33ccea7a960f39ba1cc7080b997016102bb6
H1rono <hronok66@gmail.com>
2025-02-12 12:22:33 +0900

    :adhesive_bandage: Check errno

--- tiny-ls/main.zig
+++ tiny-ls/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const c = @cImport({
-    @cInclude("errno.h");
     @cInclude("unistd.h");
 });
 
@@ -8,17 +7,26 @@ const allocator = std.heap.page_allocator;
 
 fn writeStdout(buf: []const u8) void {
     const ptr = &buf[0];
-    _ = c.write(1, ptr, buf.len);
+    const size = c.write(1, ptr, buf.len);
+    if (size < 0) {
+        const err = std.c._errno().*;
+        std.debug.panic("writing to stdout failed with errno {d}\n", .{err});
+    }
 }
 
 fn writeStderr(buf: []const u8) void {
     const ptr = &buf[0];
-    _ = c.write(2, ptr, buf.len);
+    const size = c.write(2, ptr, buf.len);
+    if (size < 0) {
+        const err = std.c._errno().*;
+        std.debug.panic("writing to stderr failed with errno {d}\n", .{err});
+    }
 }
 
 pub fn main() anyerror!void {
     var argDir: ?[]const u8 = null;
-    var args = std.process.args();
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
     for (0..2) |i| {
         const arg = args.next() orelse break;
         if (i == 1) {
@@ -27,11 +35,11 @@ pub fn main() anyerror!void {
         }
     }
     const dir = argDir orelse {
-        _ = writeStderr("Usage: tiny-ls <directory>\n");
+        writeStderr("Usage: tiny-ls <directory>\n");
         std.process.exit(1);
     };
 
     const message = try std.fmt.allocPrint(allocator, "Directory: {s}\n", .{dir});
     defer allocator.free(message);
-    _ = writeStdout(message);
+    writeStdout(message);
 }
